7f912712a0c6079a0ffe8c43e9eb568cd5c8bf24
Pavle Jonoski <jonoski.pavle@gmail.com>
2024-12-07 22:14:28 +0100

    Day 3 - Mull-It-Over :ram: :memo:

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ AdventOfCode 2024 solutions
 
 * [Day 1 - Historian Hysteria :compass: :world_map:](./day-1-historian-hysteria/)
 * [Day 2 - Red-Nosed Reports :briefcase: :clipboard:](./day-2-red-nosed-reports/)
-* [Day 3 - Mull-It-Over :construction:](./day-3-mull-it-over/)
+* [Day 3 - Mull-It-Over :ram: :memo:](./day-3-mull-it-over/)
 * [Day 4 - Ceres Search :ringed_planet: :rocket:](./day-4-ceres-search/)
--- day-3-mull-it-over/solution.py
+++ day-3-mull-it-over/solution.py
@@ -9,6 +9,14 @@ def read_input(fn):
     return ''.join(mem_lines)
 
 
+def read_input2(fn):
+    mem_lines = []
+    with open(fn) as f:
+        for line in f:
+            if line.strip() != '':
+                mem_lines.append(line.strip())
+    return mem_lines
+
 def mul_string(s):
     total = 0
     for m in re.finditer(r'mul\((?P<X>\d+),(?P<Y>\d+)\)', s):
@@ -17,78 +25,35 @@ def mul_string(s):
         total += x*y
     return total
 
+
 def part1(program):
     return mul_string(program)
 
-def tokenize(s):
-    tokens = []
 
-    buff = ''
-    i = 0
-    tkn_type = 'exec'
-    while i < len(s):
-        if s[i:].startswith('do()'):
-            if buff != '':
-                tokens.append((buff, tkn_type))
-                buff = ''
-            tkn_type = 'do'
-            i += 4
-        elif s[i:].startswith("don't()"):
-            if buff != '':
-                tokens.append((buff, tkn_type))
-                buff = ''
-            tkn_type = 'skip'
-            i+= 7
-        else:
-            buff += s[i]
-            i += 1
-    if buff != '':
-        tokens.append((buff, tkn_type))
-    return tokens
+def get_instructions(s):
+    return re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", s)
 
-def part2(program):
-    programs = []
-    with open('input') as f:
-        for line in f:
-            if line.strip() != '':
-                programs.append(line.strip())
-    on = True
-    executable = []
+def part2(programs):
+    instructions = []
     for program in programs:
-        #print('Line:', program)
-        tokens = tokenize(program)
-        #print(tokens)
-        for i, (tkn, tp) in enumerate(tokens):
-            if tp == 'exec' and on:
-                executable.append(tkn)
-            elif tp == 'do':
-                if i < len(tokens) - 1:
-                    # check if next token is also 'do'
-                    if tokens[i+1][1] == 'do':
-                        # skip this one
-                        continue
-                    else:
-                        on = True
-                        executable.append(tkn)
-                        continue
-                on = True
-                executable.append(tkn)
-            elif tp == "skip":
-                if i < len(tokens) - 1:
-                    # check if next token is also 'skip'
-                    if tokens[i+1][1] == 'skip':
-                        if on:
-                            # we can execute this one...
-                            executable.append(tkn)
-                        continue
-                    else:
-                        on = False
-                        continue
-                on = False
-
-        return sum([mul_string(exec_str) for exec_str in executable])
-
-
+        instructions += get_instructions(program)
+    
+    result = 0
+    on = True
+    for i, instr in enumerate(instructions):
+        if instr == 'do()':
+            on = True
+        elif instr == "don't()":
+            on = False
+        else:
+            m = re.match(r'mul\((\d+),(\d+)\)', instr)
+            if not m:
+                raise Exception('Invalid instruction:' + instr)
+            x = int(m.group(1))
+            y = int(m.group(2))
+            if on:
+                result += x*y
+    return result
 
 print('Part 1:', part1(read_input('input')))
-print('Part 2:', part2(read_input('input')))
+print('Part 2:', part2(read_input2('input')))
